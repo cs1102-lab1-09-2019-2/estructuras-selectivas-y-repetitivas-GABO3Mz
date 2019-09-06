b32c014d6c1a9ba118e196646989f40b877867ad
GABO3M <gabriel.espinoza@utec.edu.pe>
2019-09-05 21:29:16 -0500

    Ejercicio 6 terminado

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,23 @@
-
+#include <iostream>
+using namespace std;
 
 int main() {
+    int n, cont = 1;
+    cout << "N: ";
+    cin >> n;
+    for (int y = 0; y < n; y++){
+        for (int x = 0; x < n; x++){
+            if(cont%2 != 0){
+                cout << "# ";
+            }
+            else{
+                cout << "@ ";
+            }
+            cont++;
+        }
+        cout << "\n";
+        cont++;
+    }
 
     return 0;
 }
