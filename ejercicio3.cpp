c1e35be9cc6604c36c5142476c95de7b7c0fb602
GABO3M <gabriel.espinoza@utec.edu.pe>
2019-09-05 21:03:55 -0500

    Ejercicio 3 terminado

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,23 @@
-
+#include <iostream>
+using namespace std;
 
 int main() {
+    int num, num2, comparador = 0;
+    int i = 0;
+    cout << "n: ";
+    cin >> num2;
+
+    do
+    {
+        cout << "Num: ";
+        cin >> num;
+        if (num > comparador){
+            comparador = num;
+        }
+        i++;
+    }
+    while (i < num2);
+    cout << "El valor maximo es: " << comparador;
 
     return 0;
 }
